9306743ec02df6589e75293ec44d0e79e0eed936
botirk <14036046+botirk@users.noreply.github.com>
2025-06-05 21:12:13 +0300

    coverage

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,14 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'html', 'lcov'],
+      reporter: ['lcov'],
       provider: 'v8',
     },
     fileParallelism: false,
+    exclude: [
+      'fixtures/*',
+      'bin/*',
+      'node_modules/*'
+    ]
   },
 })
